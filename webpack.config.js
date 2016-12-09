a959772e59654a08f13916034fed70ce54056a6d
Alemneh <Alemneh@hotmail.com>
2016-12-08 18:46:39 -0800

    compressed bundle.min.js file

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,19 @@ var env = process.env.NODE_ENV || 'development';
 var CONFIG = require('./config/config')[env];
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
-
+var PROD;
+if(process.env.NODE_ENV == 'production') {
+  PROD = true;
+}
 
 var definePlugin = new webpack.DefinePlugin({
   'process.env': {
     'URL': JSON.stringify(CONFIG.host)
   }
+});
+
+var optimizeBundle = new webpack.optimize.UglifyJsPlugin({
+  compress: {warnings: false}
 });
 
 
@@ -22,7 +29,7 @@ var config = {
   ],
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js'
+    filename: 'bundle.min.js'
   },
   module: {
     loaders: [
@@ -33,7 +40,7 @@ var config = {
       }
     ]
   },
-  plugins: [definePlugin]
+  plugins: [definePlugin, optimizeBundle]
 };
 
 
